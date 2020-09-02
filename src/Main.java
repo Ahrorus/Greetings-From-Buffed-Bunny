17a7dca84d813b5f35b48652c785567f97b25d32
Ahrorus <ab.ahror@gmail.com>
2020-09-01 21:49:40 -0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("      (\\____/) '");
+        System.out.println("      (•(ㅅ)•)        Hello World");
+        System.out.println(" 　＿ノ ヽ ノ＼＿");
+        System.out.println(" /　`/ ⌒Ｙ⌒ Ｙ　ヽ");
+        System.out.println("( 　( _ヽ人 _ /　　| ");
+        System.out.println("|　 ﾉ_＼ ￣￣ヽ　 ノ");
+        System.out.println("ヽ＿＿＿＞､ _ _ ノ");
+        System.out.println("　　 ｜( 王 ﾉ〈 ");
+        System.out.println("　　 /ﾐ`― ― 彡\\");
+        System.out.println("　　/  ╰    ╯  \\");
+    }
+
+}
